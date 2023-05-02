e68f7ec9b051a7816b3e3c2594604a61bc09f640
max <maximilian.filipov@stud.fra-uas.de>
2023-05-02 16:27:27 +0200

    oij

--- main.py
+++ main.py
@@ -11,12 +11,38 @@ from scraper import *
 json_path = "json"
 test_path = "test_dir"
 
+
 def usage():
     print("""
      Usage: ./main.py [--option]
             ./main.py --fetch -> fetch all json files
             ./main.py --init  -> initialize database with content
             """)
+
+
+def fetch_json_pages():
+    # get number of pages to fetch
+    url = 'https://storage.googleapis.com/fra-uas-mobappex-ss23-amin/ard-feed-0.json'
+    req = requests.get(url)
+    content = json.loads(req.text)
+    pages = content['totalPageCount']
+
+    url_to_fetch = 'https://storage.googleapis.com/fra-uas-mobappex-ss23-amin/ard-feed-{}.json'
+    # for p in range(int(pages)):
+    #     fetch_url = url_to_fetch.format(p)
+    #     print(f"fetching: {fetch_url}")
+    #     req = requests.get(fetch_url)
+    #     with open(f"json/json_file{p}.json", "w") as fd:
+    #         fd.write(req.text)
+
+    # improve performance instead of writing to 153 files write to one big one
+    # so no need for 153 open / close calls io are slow
+
+    with open("data.json", "a") as fd:
+        for p in range(int(pages)):
+            fetch_url = url_to_fetch.format(p)
+            req = requests.get(fetch_url)
+            fd.write(req.text)
 
 
 class OptionMenu:
@@ -39,28 +65,13 @@ class OptionMenu:
                 os.makedirs(test_path)
                 print("created directory test")
 
-            self.fetch_json_pages()
+            fetch_json_pages()
 
         elif args.init:
             self.items.initialize().print()
         else:
             usage()
             sys.exit(1)
-
-    def fetch_json_pages(self):
-        # get number of pages to fetch
-        url = 'https://storage.googleapis.com/fra-uas-mobappex-ss23-amin/ard-feed-0.json'
-        req = requests.get(url)
-        content = json.loads(req.text)
-        pages = content['totalPageCount']
-
-        url_to_fetch = 'https://storage.googleapis.com/fra-uas-mobappex-ss23-amin/ard-feed-{}.json'
-        for p in range(int(pages)):
-            fetch_url = url_to_fetch.format(p)
-            print(f"fetching: {fetch_url}")
-            req = requests.get(fetch_url)
-            with open(f"json/json_file{p}.json", "w") as fd:
-                fd.write(req.text)
 
 
 if __name__ == '__main__':
